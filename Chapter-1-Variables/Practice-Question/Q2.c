709d4069382e54579d2f6d494185dcc8f12e95e2
Chaitali Shirsath <chaitali3611gmail.com>
2025-12-01 22:38:21 +0530

    Area-of-room

--- Chapter-1-Variables/Practice-Question/Q2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    float r;
-    printf("Enter the radius of the circle :");
-    scanf("%f", &r);
-    printf("Area of the circle is %f", 3.14*r*r);
-    return 0;
-}
